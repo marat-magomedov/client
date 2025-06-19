ee508c8473dbb913db6df0c9ba1414bfa388caa6
marat-magomedov <imagomedov.marat@gmail.com>
2025-06-19 20:36:41 +0300

    fixes 9.0

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://176.124.213.151',
+        target: 'https://176.124.213.151',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
