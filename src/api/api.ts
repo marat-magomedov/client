9f9c4d85158590c8aeb616f357ba87b703a02a52
marat-magomedov <imagomedov.marat@gmail.com>
2025-06-19 18:31:28 +0300

    fixes 1.0

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api/';
+const API_URL = 'https://marat-magomedov-server-6143.twc1.net/api/';
 
 export const api = axios.create({
   baseURL: API_URL,
--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'https://marat-magomedov-server-6143.twc1.net',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
